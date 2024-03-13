7fdd59f9f7c3de1474fbff45cae4c409a9165d22
harry watton <harry.watton@hotmail.co.uk>
2024-03-13 21:43:18 +0000

    typo

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-full flex-col items-center justify-between p-24">
       <div>This is a test application using NextJS & material-tailwind component library</div>
-      <div>{`It will also incorporate some insteraction with AWS, all being well ;)`}</div>
+      <div>{`It will also incorporate some interaction with AWS, all being well ;)`}</div>
       <CustomButton value={'some ludicrous value'}/>
      </main>
   )
